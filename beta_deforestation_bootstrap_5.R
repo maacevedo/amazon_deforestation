15be363a5a704187a3ef7cb466b3807b075c947b
Morgan Manning <60010440+morganmanning@users.noreply.github.com>
2025-03-26 08:56:36 -0400

    Clean up formatting (no programming changes made)

--- beta_deforestation_bootstrap_5.R
+++ beta_deforestation_bootstrap_5.R
@@ -1,8 +1,9 @@
 ###############################
-#Beta regression analysis
-#Programmer: Miguel Acevedo
+# Beta regression analysis
+# Programmer: Miguel Acevedo
 ##################################
 
+# Load the necessary packages
 library(ggplot2)
 library(lme4)
 library(sjPlot)
@@ -18,55 +19,77 @@ library(TMB)
 library(boot)
 library(ggtext)
 
-#Linear model
 
-data <- read.csv("Dec30_Input_Output_ALL-1.csv",header=TRUE,
-                 stringsAsFactors = TRUE) #input data from Walker provided in Dec24
+################################################################################
+############################ DATA PREPARATION ##################################
+################################################################################
 
-head(data) #look at the data
+# Load necessary data
+data <- read.csv("Dec30_Input_Output_ALL-1.csv",
+    header = TRUE,
+    stringsAsFactors = TRUE
+) # input data from Walker provided in Dec2024
+
+# Look at the data
+head(data)
 names(data)
 
-data$defit_prop=data$defit/100 #turn percentages into proportions
+# Turn percentages into proportions
+data$defit_prop <- data$defit/100 # proportion of deforestation inside IT
+data$def50_prop <- data$def50/100 # proportion of deforestation at 50km buffer
+# Note: 50km buffer, but can be changed to other buffers like def100 or def10
 
-data$def50_prop=data$def50/100 #50km buffer, but can be changed to other buffers like def100 or def10
+# Ensure groups are factors to use as random effect
+data$group <- as.factor(data$group) 
 
-#ensure groups are factors to use as RE
-data$group=as.factor(data$group) 
 
-#run model with polynomial deterministic function of degree 2
-#beta distribution with logit link function (i.e., log (p/1-p))
-mod_beta_re <- glmmTMB(defit_prop~def50_prop+I(def50_prop^2)+
-                         (1|group),data=data,control = glmmTMBControl(rank_check = "adjust"),
-                       family=beta_family(link="logit"))
 
-#adding scale(size_km^2) does not significantly improve model fit
+################################################################################
+############################ MODEL FITTING #####################################
+################################################################################
 
-#Check model fit
-r2(mod_beta_re)#conditional R^2 = 0.41
+# Run model with polynomial deterministic function of degree 2
+# Beta distribution with logit link function (i.e., log (p/1-p))
+mod_beta_re <- glmmTMB(defit_prop ~ def50_prop + I(def50_prop^2) + (1 | group),
+    data = data,
+    control = glmmTMBControl(rank_check = "adjust"),
+    family = beta_family(link = "logit")
+)
 
-########################
-#Visualization
-#######################
+# Note: Adding scale(size_km^2) does not significantly improve model fit
 
-#make model predictions
-preds_re=as.data.frame(effect("def50_prop",mod_beta_re,xlevels=list(def50_prop=seq(0,1,by=0.01))))
+# Check model fit
+r2(mod_beta_re) # Conditional R^2 = 0.410
 
-#plot model predictions with 1:1 line representing H0
+
+
+################################################################################
+############################ VISUALIZATION #####################################
+################################################################################
+
+# Make model predictions
+preds_re <- as.data.frame(effect("def50_prop", # Predictor variable
+    mod_beta_re, # Model object
+    xlevels = list(def50_prop = seq(0, 1, by = 0.01)) # Sequence of deforestation proportions to predict across
+))
+
+# Plot model predictions with 1:1 line representing H0
 p_beta_re <- ggplot(preds_re, aes(x = def50_prop, y = fit)) +
   geom_line() +
   geom_smooth(aes(ymin = lower, ymax = upper), stat = "identity") +
   geom_point(data = data, aes(x = def50_prop, y = defit_prop, size = size_km2))+
   scale_size_continuous(name = expression("Territory Size (Km"^2*")"))+
-  geom_abline(slope = 1, intercept = 0, linetype="dashed", color="blue")+
+  geom_abline(slope = 1, intercept = 0, linetype = "dashed", color = "blue")+
   xlab("Deforestation at 50km buffer")+
   ylab("Deforestation inside IT")+
   coord_cartesian(ylim = c(0,1))+
   theme_bw()
 
-##########################################
-#Deforestation predictions: Simulation
-##########################################
- 
+
+
+################################################################################
+################## DEFORESTATION PREDICTIONS: SIMULATION #######################
+################################################################################
 
 # Function to simulate from the fitted model
 nsim <- 1000  # number of simulations
@@ -88,22 +111,31 @@ point_estimate <- sum((data$def50_prop-fitted_vals)*data$size_km2)
 print(paste("Sum of differences:", round(point_estimate, 2)))
 print(paste("95% CI:", round(ci[1], 2), "to", round(ci[2], 2)))
 
-######################################
-#Deforestation predictions: Bootstrap
-#####################################
+
+
+################################################################################
+################### DEFORESTATION PREDICTIONS: BOOTSTRAP #######################
+################################################################################
 
 boot_func <- function(data, indices) {
-  # Resample data
-  d <- data[indices,]
-  
-  # Fit model to resampled data
-  tryCatch({
-    mod <- glmmTMB(defit_prop~def50_prop+I(def50_prop^2)+
-                     (1|group),data=data,control = glmmTMBControl(rank_check = "adjust"),
-                   family=beta_family(link="logit"))
-    pred <- predict(mod, type = "response")
-    return(sum((d$def50_prop - pred)*d$size_km2))
-  }, error = function(e) NA)
+    # Resample data
+    d <- data[indices, ]
+
+    # Fit model to resampled data
+    tryCatch(
+        {
+            mod <- glmmTMB(
+                defit_prop ~ def50_prop + I(def50_prop^2) +
+                    (1 | group),
+                data = data,
+                control = glmmTMBControl(rank_check = "adjust"),
+                family = beta_family(link = "logit")
+            )
+            pred <- predict(mod, type = "response")
+            return(sum((d$def50_prop - pred) * d$size_km2))
+        },
+        error = function(e) NA
+    )
 }
 
 # Perform bootstrap
@@ -111,10 +143,11 @@ group_ids <- data$group
 boot_results <- boot(data = data, 
                      statistic = boot_func, 
                      R = 1000,
-                     strata=group_ids)
+                     strata = group_ids)
 
 # Calculate bootstrap confidence intervals
 boot_ci <- boot.ci(boot_results, type = "basic")
 
+# Print results
 print("Bootstrap results:")
 print(boot_ci)
--- neutral.R
+++ neutral.R
@@ -1,14 +1,24 @@
+###############################
+# Beta regression analysis
+# Programmer: Miguel Acevedo
+##################################
+
+# Load the necessary packages
 library(readxl)
 
+# Load the data
 neutral_data <- read_excel("Neutral Landscape with defit def50.xlsx")
 
+# Look at the data
 hist(neutral_data$defit)
 hist(neutral_data$def50)
 
-plot(neutral_data$def50,neutral_data$defit)
-lines(lowess(neutral_data$def50,neutral_data$defit),col="blue")
+# Plot deforestation inside IT vs deforestation at 50km buffer
+plot(neutral_data$def50, neutral_data$defit)
+lines(lowess(neutral_data$def50, neutral_data$defit), col = "blue")
 
-mod <- lm(defit~def50,data=neutral_data)
+# Fit a linear model
+mod <- lm(defit ~ def50, data = neutral_data)
 summary(mod)
 
 
